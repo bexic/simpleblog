dfe12fe95cbba8918c683004f015b921d749d341
Pourya Da <dashtegolipour@strands.com>
2018-03-09 12:08:33 +0800

    minor fix: add sample posts

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,5 +1,27 @@
 export default {
-  posts: {},
+  posts: [
+    {
+      "id": 1,
+      "title": "Egypt",
+      "body": "Egypt, a country linking northeast Africa with the Middle East, dates to the time of the pharaohs. Millennia-old monuments sit along the fertile Nile River Valley, including Giza's colossal Pyramids and Great Sphinx as well as Luxor's hieroglyph-lined Karnak Temple and Valley of the Kings tombs. The capital, Cairo, is home to Ottoman landmarks like Muhammad Ali Mosque and the Egyptian Museum, a trove of antiquities.",
+      "tag": "country, pyramid",
+      "image": "https://goo.gl/5uZo71"
+    },
+    {
+      "id": 2,
+      "title": "England",
+      "body": "England, birthplace of Shakespeare and The Beatles, is a country in the British Isles bordering Scotland and Wales. The capital, London, on the River Thames, is home of Parliament, Big Ben and the 11th-century Tower of London. It's also a multicultural, modern hub for the arts and business. Other large cities are Manchester, Birmingham, Liverpool, Bristol and the university centres of Oxford and Cambridge.",
+      "tag": "country, london",
+      "image": "https://goo.gl/tfh5ci"
+    },
+    {
+      "id": 3,
+      "title": "Singapore",
+      "body": "Singapore, an island city-state off southern Malaysia, is a global financial center with a tropical climate and multicultural population. Its colonial core centers on the Padang, a cricket field since the 1830s and now flanked by grand buildings such as City Hall, with its 18 Corinthian columns. In Singapore's circa-1820 Chinatown stands the red-and-gold Buddha Tooth Relic Temple, said to house one of Buddha's teeth.",
+      "tag": "country, Marina Bay",
+      "image": "https://goo.gl/k4EkwU"
+    }
+  ],
   postActive: {
     title: 'Select a post to see the details',
     body: 'oops...!',
